a8eb6228897934329863725abce988403973f57b
Corey Skidmore <skidmoreeco@gmail.com>
2022-09-19 20:51:45 -0400

    FINAL TOUCHES

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "Apache") {
+    return `[[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0]`
+  }
+  if (license === "Boost") {
+    return `[[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)]`
+  }
+
+  if (license === "BSD"){
+    return `[[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)]`
+  }
+
+  if (license === "MIT"){
+    return `[[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)]`
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -55,8 +71,8 @@ ${data.tests}
 
 ## Questions
 
-<a href = "h
-<a href 
+<a href = "https://github.com/${data.github}" > Github Profile </a><br>
+<a href = "mailto:${data.email}" > Email Me </a>
 
 ## Email
 
